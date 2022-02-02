967b6fc2472a9c14653104f635efab2ed9c00af5
Aleksey-Smouzh <xmz@tut.by>
2022-02-02 15:31:40 +0300

    unit4/4

--- unit_04/unit_04.js
+++ unit_04/unit_04.js
@@ -42,6 +42,18 @@ document.querySelector('.p-3').onclick = f3;
 // При клике по кнопке .b-4 запускается функция f4. Функция проверяет состояние .i-4 и если он checked - выводит true, если не выбран - false. Вывод везде в задачах, где не указано другое, осуществляется в div.out-номер задачи. В данном случае - div.out-4
 
 function f4() {
+const b4 = document.querySelector('.b-4').onclick = () =>{
+    let out4 = document.querySelector('.out-4');
+    let i4 = document.querySelector('.i-4')
+    console.log(i4.checked)
+    if (i4.checked){
+        out4.innerHTML = "div.out-4";
+    }else{
+
+    }
+   
+}
+
 
 }
 
