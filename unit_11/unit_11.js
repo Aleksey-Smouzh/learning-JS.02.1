fdcaaa7a9e15468dc3d0b9d93e02ccf5b9744d7d
Aleksey-Smouzh <xmz@tut.by>
2022-02-09 13:16:36 +0300

    unit11/3

--- unit_11/unit_11.js
+++ unit_11/unit_11.js
@@ -26,7 +26,7 @@ document.querySelector(".b-1").onclick = f1;
 // функция выполняется при нажатии кнопки b-2
 
 function f2() {
-    d1.pop()
+  d1.pop();
   showArr(".out-2", d1);
 }
 
@@ -37,6 +37,7 @@ document.querySelector(".b-2").onclick = f2;
 // функция выполняется при нажатии кнопки b-3
 
 function f3() {
+  d1.shift();
   showArr(".out-3", d1);
 }
 
