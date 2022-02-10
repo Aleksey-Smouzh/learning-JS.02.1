efbf338930e98790eddd455aa54bf87bf5aab616
Aleksey-Smouzh <xmz@tut.by>
2022-02-10 16:25:55 +0300

    unit12/13

--- unit_12/unit_12.js
+++ unit_12/unit_12.js
@@ -250,7 +250,24 @@ document.querySelector(".b-12").onclick = f12;
 
 let a13 = [];
 
-function f13() {}
+function f13() {
+  a13 = [];
+  let p = 0;
+  for (let i = 0; i < 8; i++) {
+    let t = [];
+    for (let k = 0; k < 8; k++) {
+      if (p % 2 === 0) {
+        t.push(1);
+      } else {
+        t.push(0);
+      }
+       p++;
+    }
+    a13.push(t);
+     p++;
+  }
+  console.log(a13);
+}
 
 document.querySelector(".b-13").onclick = f13;
 
