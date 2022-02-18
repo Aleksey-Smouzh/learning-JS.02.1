3a660855300acf8baa8a3369fff2d53710b12d09
Aleksey-Smouzh <xmz@tut.by>
2022-02-18 22:02:05 +0300

    unit17/5

--- unit_17/unit_17.js
+++ unit_17/unit_17.js
@@ -95,7 +95,13 @@ document.querySelector('.b-4').onclick = () => {
 let a5 = [3, 14, 15, 92, '6'];
 
 function t5() {
-   
+  a5_res = a5.filter(function(elem){
+      if(elem % 2 == 0 && typeof elem === "number"){
+          return true
+
+      }
+  })
+  return a5_res 
 }
 
 document.querySelector('.b-5').onclick = () => {
