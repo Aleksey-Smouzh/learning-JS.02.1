6f395415db24553dcbfe948686ae948f22c0eb60
Aleksey-Smouzh <xmz@tut.by>
2022-02-22 12:37:11 +0300

    unit18/5

--- unit_18/unit_18.js
+++ unit_18/unit_18.js
@@ -62,6 +62,11 @@ document.querySelector('.b-4').onclick = t4;
 /*  На странице созданы 3 p.task-5 c атрибутом data. С помощью forEach переберите их и добавьте событие клик. Напишите функцию t5, которая будет запускаться при клике и добавлять атрибут data элемента, по которому кликнули в массив a5_res.*/
 
 function t5() {
+    // let task = document.querySelectorAll('.task-5')
+    
+        a5_res.push(this.getAttribute('data'))
+        document.querySelector('.out-5').innerHTML = a5_res
+    
 
 }
 
