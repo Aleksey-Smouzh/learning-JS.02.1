49b79d5a1a05dae5833ef2a4c32c73079fda226f
Aleksey-Smouzh <xmz@tut.by>
2022-02-22 13:20:56 +0300

    unit18/7

--- unit_18/unit_18.js
+++ unit_18/unit_18.js
@@ -80,7 +80,7 @@ document.querySelectorAll('.task-5').forEach(elem => {
 /*  Дана строка str6='helloworld' - преобразуйте ее в массив и присвойте a6_res. Выведите на страницу. Запускаться решение должно при вызове функции t6. */
 str6='helloworld'
 function t6() {
- a6_res = (str6.split(''))
+ a6_res = str6.split('')
  console.log(a6_res)
 document.querySelector('.out-6').innerHTML =a6_res
 }
@@ -92,7 +92,12 @@ document.querySelector('.b-6').onclick = t6;
 /*  Дана строка str7='hello world hi mazai' - преобразуйте ее в массив и разбейте по словам. Причем слова не должны содержать пробелов и присвойте a6_res. выведите на страницу.  Запускаться решение должно при вызове функции t7. */
 
 function t7() {
+    str7='hello world hi mazai'
+   a7_res = (str7.split(" "))
+ 
 
+console.log(a7_res)
+document.querySelector('.out-7').innerHTML = a7_res
 }
 
 document.querySelector('.b-7').onclick = t7;
