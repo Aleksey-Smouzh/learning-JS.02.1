b5e317e001deb9fcf4cd5c83bd510b036f6863ee
Aleksey-Smouzh <xmz@tut.by>
2022-02-22 13:00:25 +0300

    unit18/6

--- unit_18/unit_18.js
+++ unit_18/unit_18.js
@@ -78,9 +78,11 @@ document.querySelectorAll('.task-5').forEach(elem => {
 
 // Task 6 ============================================
 /*  Дана строка str6='helloworld' - преобразуйте ее в массив и присвойте a6_res. Выведите на страницу. Запускаться решение должно при вызове функции t6. */
-
+str6='helloworld'
 function t6() {
-
+ a6_res = (str6.split(''))
+ console.log(a6_res)
+document.querySelector('.out-6').innerHTML =a6_res
 }
 
 document.querySelector('.b-6').onclick = t6;
