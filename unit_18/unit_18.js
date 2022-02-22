4ea2f6115775ad16c3fdb572fc1ef80d47461be3
Aleksey-Smouzh <xmz@tut.by>
2022-02-22 13:31:30 +0300

    unit18/8

--- unit_18/unit_18.js
+++ unit_18/unit_18.js
@@ -106,7 +106,9 @@ document.querySelector('.b-7').onclick = t7;
 /*  Дан массив a8 = [1,2,66,77,15] - преобразуйте ее в строку. Разделитель - дефис. Результат присвойте a8_res. Запускаться решение должно при вызове функции t8. */
 
 function t8() {
-
+    a8 = [1,2,66,77,15]
+    a8_res = (a8.join("-"))
+    document.querySelector('.out-8').innerHTML = a8_res
 }
 
 document.querySelector('.b-8').onclick = t8;
