a8e8928293fc73217e878f321b8d492f0ed3f0f8
Aleksey-Smouzh <xmz@tut.by>
2022-02-23 12:39:39 +0300

    unit19/7

--- dz/unit_19.js
+++ dz/unit_19.js
@@ -67,8 +67,11 @@ document.querySelector('.div-6').ondblclick = t6
 // Task 7 ============================================
 /*  Дан блок .div-7. При клике правой кнопкой мыши на блоке добавляйте ему класс .active. При повторном клике - удаляйте. */
 
-function t7() {}
+function t7(e) {
+ return (e.target.classList.toggle('active'));
 
+}
+document.querySelector('.div-7').oncontextmenu = t7
 // ваше событие здесь!!!
 
 // Task 8 ============================================
